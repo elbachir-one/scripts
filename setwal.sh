9125297acc35351b4da0655f406638809f8468bf
elbachir-one <bachiralfa@gmail.com>
2025-10-13 17:31:10 +0100

    Replaced walmenu.sh with setwal.sh

--- setwal.sh
+++ setwal.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-FOLDER="$HOME/wall"                     # Wallpapers path
-WAL_FILE="$HOME/.cache/wal/colors-wal-dwm.h"  # Colors path
-DWM_DIR="$HOME/suckless/dwm"           # Path to dwm directory
+# Set the colors for dwm using pywal
 
-# Build menu with wallpapers
+FOLDER="$HOME/wall"
+WAL_FILE="$HOME/.cache/wal/colors-wal-dwm.h"
+DWM_DIR="$HOME/suckless/dwm"
+
 CHOICE=$(printf "Random\n%s" "$(ls -1 "$FOLDER")" | dmenu -l 15 -i -p "Wallpaper: ")
 
 case "$CHOICE" in
@@ -19,17 +20,13 @@ case "$CHOICE" in
 		;;
 esac
 
-# Apply wallpaper with pywal and feh
 wal -i "$IMG"
 feh --bg-fill "$IMG"
 
 # Patch pywal dwm header
 if [[ -f "$WAL_FILE" ]]; then
-	# Remove SchemeUrg entries
 	sed -i '/SchemeUrg/d' "$WAL_FILE"
-	# Remove urg_* declarations
 	sed -i '/^static const char urg_/d' "$WAL_FILE"
-	# Remove 'const' from all static char declarations
 	sed -i 's/static const char/static char/g' "$WAL_FILE"
 	echo "Patched $WAL_FILE for dwm"
 fi
