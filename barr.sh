42c02ac2f96cb0d1266bf82ac7a5668851130b71
elbachir-one <bachiralfa@gmail.com>
2025-10-14 14:31:49 +0100

    Remove deleted files

--- barr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/barrierc -f --no-tray --debug INFO --name VOID --disable-crypto 192.168.1.10:24800
--- co.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dir=$1
-find "$dir" -type f -exec wc -l {} + | awk '{s+=$1} END {print s}'
