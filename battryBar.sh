88653930dd486512f6d0cba958eced07844ccd65
elbachir-one <bachiralfa@gmail.com>
2025-11-01 01:59:30 +0100

    Colored battery status

---
+++ battryBar.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Battery status with a bar and colors
+
+perc=$(cat /sys/class/power_supply/BAT1/capacity)
+
+total=5
+
+filled=$(( perc * total / 100 ))
+
+bar=""
+for ((i=0; i<total; i++)); do
+	if [ $i -lt $filled ]; then
+		bar+="█"
+	else
+		bar+="░"
+	fi
+done
+
+if [ "$perc" -ge 50 ]; then color="\e[32m"
+elif [ "$perc" -ge 20 ]; then color="\e[33m"
+else color="\e[31m"
+fi
+
+echo -e "Battery: ${color}[$bar]\e[0m $perc%"
